5335129938bcf199800c5a8f56f465541fffa256
Sam Ruby <akashgupta@blubirch.com>
2023-02-21 10:41:19 +0530

    finishing up

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,22 @@ Rails.application.routes.draw do
     post 'login' => :create
     delete 'logout' => :destroy
   end
+  get 'sessions/create'
+  get 'sessions/destroy'
+
+  resources :support_requests, only: [ :index, :update ]
+  
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
+  
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
